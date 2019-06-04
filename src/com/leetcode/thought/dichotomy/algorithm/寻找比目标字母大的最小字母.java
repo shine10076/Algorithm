e3b71ce86bdb236a0cc3fb3507d37abdefa42044
shine10076 <1007623893@qq.com>
2019-06-04 20:59:51 +0800

    dichotomy algorithm

---
+++ src/com/leetcode/thought/dichotomy/algorithm/寻找比目标字母大的最小字母.java
@@ -0,0 +1,36 @@
+package com.leetcode.thought.dichotomy.algorithm;
+
+/**
+ * @author shine10076
+ * @date 2019/6/2 13:44
+ */
+public class 寻找比目标字母大的最小字母 {
+
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int L = 0, R = letters.length-1;
+        char res = 'z';
+        boolean flag = false;
+        while(L<=R)
+        {
+            int mid = L + ((R-L)>>1);
+            if(letters[mid]>target)
+            {
+                if(letters[mid] <= res)
+                {
+                    res = letters[mid];
+                    flag = true;
+                }
+                R = mid -1;
+            }else
+            {
+                L = mid + 1;
+            }
+        }
+        return flag?res:letters[0];
+    }
+
+    public static void main(String[] args) {
+        char[] letters = new char[]{'c', 'f', 'j'};
+        System.out.println(nextGreatestLetter(letters,'j'));
+    }
+}
