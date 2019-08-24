6493202db56216f2697e9ec590656d74943068a4
shine10076 <1007623893@qq.com>
2019-08-24 14:06:02 +0800

    offer algorithm

---
+++ src/com/offer/algorithm/正则表达式匹配.java
@@ -0,0 +1,13 @@
+package com.offer.algorithm;
+
+/**
+ * @author shine10076
+ * @date 2019/8/24 14:04
+ */
+public class 正则表达式匹配 {
+
+    public boolean match(char[] str, char[] pattern)
+    {
+        return true;
+    }
+}
