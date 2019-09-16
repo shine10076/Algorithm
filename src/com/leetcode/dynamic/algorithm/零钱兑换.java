8a8b5cccb27c7fd5ac15b814c28c6d0c0c6c0aab
shine10076 <1007623893@qq.com>
2019-09-16 20:53:50 +0800

    dynamic algorithm

--- src/com/leetcode/dynamic/algorithm/零钱兑换.java
+++ src/com/leetcode/dynamic/algorithm/零钱兑换.java
@@ -8,61 +8,47 @@ public class 零钱兑换 {
 
     public static int coinChange(int[] coins, int amount) {
 
+
+        if(coins == null || coins.length == 0 || amount < 0)
+        {
+            return -1;
+        }
         int n = coins.length;
         int[][] money = new int[n][amount+1];
+        int max = Integer.MAX_VALUE;
 
-        for(int i=0;i<money.length;i++)
-            for(int j=1;j<amount+1;j++)
-            {
-                money[i][j] = Integer.MAX_VALUE;
-            }
-
-        for(int i=0;i<n;i++)
+        for(int j = 1; j<=amount;j++)
         {
-            for(int j=1;j*coins[i]<=amount;j++)
+            money[0][j] = max;
+            if(j-coins[0]>=0 && money[0][j-coins[0]]!= max)
             {
-                money[i][j*coins[i]] = 1;
+                money[0][j] = money[0][j-coins[0]]+1;
             }
         }
 
-
-
+        int left = 0;
         for(int i=1;i<n;i++)
         {
             for(int j=1;j<=amount;j++)
             {
-                if((j-coins[i])<=0) continue;
-                for(int k=0;j-coins[i]*k>0;k++)
+                left = max;
+                if(j-coins[i]>=0 && money[i][j-coins[i]] != max )
                 {
-
-                    money[i][j] = Math.min(money[i-1][j-coins[i]*k]+k,money[i][j]);
+                    left = money[i][j-coins[i]] + 1;
                 }
+                money[i][j] = Math.min(left,money[i-1][j]);
             }
         }
 
-        int res=0;
-        for(int i=0;i<n;i++)
-        {
-            res = Math.min(money[i][amount],res);
-        }
 
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<=amount;j++)
-            {
-                System.out.print(money[i][j]+" ");
-            }
-            System.out.println();
-        }
-
-        return res;
+        return money[n-1][amount]!=max?money[n-1][amount]:-1;
 
     }
 
 
     public static void main(String[] args) {
-        int[] coins = {1,2,5};
-        int amount = 11;
-        coinChange(coins,amount);
+        int[] coins = {186,419,83,408};
+        int amount = 6249;
+        System.out.println(coinChange(coins,amount));
     }
 }
