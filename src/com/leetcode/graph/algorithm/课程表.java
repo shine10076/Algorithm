582895be6f2bc4809dfe1499fa8f11ca072632c8
shine10076 <1007623893@qq.com>
2019-10-14 21:00:09 +0800

    graph algorithm

---
+++ src/com/leetcode/graph/algorithm/课程表.java
@@ -0,0 +1,80 @@
+package com.leetcode.graph.algorithm;
+
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Stack;
+
+/**
+ * @author shine10076
+ * @date 2019/10/14 19:54
+ */
+public class 课程表 {
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+        ArrayList<Node> list = new ArrayList<>();
+        for(int i=0;i<numCourses;i++)
+        {
+            Node node = new Node(i);
+            list.add(node);
+        }
+        /**
+         * 添加依赖关系
+         */
+        for(int[] nums: prerequisites)
+        {
+            int end = nums[0];
+            int start = nums[1];
+            list.get(start).nexts.add(list.get(end));
+        }
+
+        boolean res = true;
+        for(Node node : list)
+        {
+            res &= isCycle(node);
+        }
+
+        return res;
+    }
+
+
+    public boolean isCycle(Node node)
+    {
+        if(node == null) return false;
+        Stack<Node> stack = new Stack<>();
+        HashSet<Node> set = new HashSet<>();
+        stack.push(node);
+        set.add(node);
+        Node thisNode = node;
+        while (!stack.isEmpty())
+        {
+            Node cur = stack.pop();
+            for(Node next : cur.nexts)
+            {
+                if(next == node) return false;
+                if(!set.contains(next))
+                {
+                    stack.push(cur);
+                    stack.push(next);
+                    set.add(next);
+                    break;
+                }
+            }
+        }
+        return true;
+    }
+
+
+    class Node {
+
+        public Integer id;
+        public ArrayList<Node> nexts = new ArrayList<>();
+
+        public Node(int id)
+        {
+            this.id = id;
+        }
+
+    }
+}
