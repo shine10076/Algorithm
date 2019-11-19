2a7e8b83bbb273b2256c0318ecceeeb0cef776e1
shine10076 <1007623893@qq.com>
2019-11-19 20:55:17 +0800

    tree algorithm

---
+++ src/com/leetcode/tree/algorithm/Node.java
@@ -0,0 +1,21 @@
+package com.leetcode.tree.algorithm;
+
+/**
+ * @author shine10076
+ * 2019/11/19 20:47
+ */
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val,Node _left,Node _right,Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
