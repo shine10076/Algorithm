3ceaa2321935278839014d0b121fa362de649a23
shine10076 <1007623893@qq.com>
2019-12-04 20:51:52 +0800

    Tree algorithm

--- src/com/leetcode/tree/algorithm/二叉搜索树的最小绝对差值.java
+++ src/com/leetcode/tree/algorithm/二叉搜索树的最小绝对差值.java
@@ -10,26 +10,25 @@ import java.util.Stack;
  */
 public class 二叉搜索树的最小绝对差值 {
 
+    TreeNode preNode = null;
+    int minAbsDiff = Integer.MAX_VALUE;
     public int getMinimumDifference(TreeNode root) {
-        /*中序遍历是有序的*/
-        List<Integer> list = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode cur = root;
-        while (cur != null || !stack.isEmpty()) {
-            while (cur != null) {
-                stack.push(cur);
-                cur = cur.left;
-            }
 
-            TreeNode node = stack.pop();
-            list.add(node.val);
-            cur = node.right;
+        preOrder(root);
+        return minAbsDiff;
+    }
+
+    public void preOrder(TreeNode root){
+        if(root == null ) return;
+        preOrder(root.left);
+
+        if(preNode != null){
+            int diff = Math.abs(preNode.val - root.val);
+            minAbsDiff = Math.min(diff,minAbsDiff);
         }
-        int min = Integer.MAX_VALUE;
-        for (int i = 1; i < list.size(); i++) {
-            int num = Math.abs(list.get(i) - list.get(i - 1));
-            if (num < min) min = num;
-        }
-        return min;
+
+        preNode = root;
+        preOrder(root.right);
+
     }
 }
