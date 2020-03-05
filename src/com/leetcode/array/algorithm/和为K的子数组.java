449f2b6340241eacb3fc6a956f1ba1d63fd426fb
shine10076 <1007623893@qq.com>
2020-03-05 16:55:31 +0800

    array algorithm

--- src/com/leetcode/array/algorithm/和为K的子数组.java
+++ src/com/leetcode/array/algorithm/和为K的子数组.java
@@ -1,5 +1,7 @@
 package com.leetcode.array.algorithm;
 
+import java.util.HashMap;
+
 /**
  * @author shine10076
  * @date 2019/10/17 19:52
@@ -32,4 +34,25 @@ public class 和为K的子数组 {
         return res;
     }
 
+
+    //采用hash表
+    public int subarraySum2(int[] nums, int k) {
+
+        /**
+         * key : nums[0...i]的和的值 k
+         * value ： 值为k的nums[0...i]的i的数量
+         */
+        int res = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        map.put(sum,1);
+        for(int i : nums){
+            sum += i;
+            if(map.containsKey(sum-k)){
+                res += map.get(sum-k);
+            }
+            map.put(sum, map.getOrDefault(sum,0)+1);
+        }
+        return res;
+    }
 }
