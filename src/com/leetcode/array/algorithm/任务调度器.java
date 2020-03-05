595d42619d782aec4f2880c4fe1dabe260417a5c
shine10076 <1007623893@qq.com>
2020-03-05 15:26:08 +0800

    array algorithm

--- src/com/leetcode/array/algorithm/任务调度器.java
+++ src/com/leetcode/array/algorithm/任务调度器.java
@@ -1,7 +1,6 @@
 package com.leetcode.array.algorithm;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * @author shine10076
@@ -10,28 +9,59 @@ import java.util.Queue;
 public class 任务调度器 {
 
     public int leastInterval(char[] tasks, int n) {
-
-        Queue<Character> queue = new LinkedList<>();
+        int time = 0;
         int[] letter = new int[26];
-        for(char c : tasks)
-        {
+
+        for(char c : tasks){
             letter[c-'A']++;
         }
-        boolean flag = true;
-        while (flag)
-        {
-            flag = false;
-            for(int i=0;i<26;i++)
-            {
-                if(letter[i]!=0)
-                {
-                    letter[i]--;
-                    queue.offer((char)('A'+i));
-                    flag = true;
-                }
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2-o1;
+            }
+        });
+
+        for(int i=0;i<26;i++){
+            if(letter[i]>0){
+                priorityQueue.offer(letter[i]);
             }
         }
 
-        return 0;
+        while (!priorityQueue.isEmpty()){
+            List<Integer> list = new ArrayList<>();
+            for(int i=0;i<=n;i++){
+                if((!priorityQueue.isEmpty())){
+                    if(priorityQueue.peek() > 1){
+                        list.add(priorityQueue.poll()-1);
+                    }else {
+                        priorityQueue.poll();
+                    }
+                }
+                time++;
+                if(list.size() == 0 && priorityQueue.isEmpty()) break;
+
+            }
+            for(Integer i : list){
+                priorityQueue.offer(i);
+            }
+        }
+
+        return time;
+
+    }
+
+    public static void main(String[] args) {
+        PriorityQueue<Integer> integers = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2-o1;
+            }
+        });
+        integers.offer(3);
+        integers.offer(2);
+        System.out.println(integers.poll());
+        System.out.println(integers.poll());
     }
 }
